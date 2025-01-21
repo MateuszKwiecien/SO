c9e486281730cc8268b45ac5345ac8e75cb183f1
Mateusz Kwiecien <mateo_kw@outlook.com>
2025-01-21 04:26:15 +0100

    Implemented assemble and supply function

--- processes/assembler.cpp
+++ processes/assembler.cpp
@@ -2,15 +2,21 @@
 
 using namespace std;
 
+void assemble_product(Warehouse* warehouse){
+    if(warehouse->X && warehouse->Y && warehouse->Z){   // Checks if all required components are available
+        warehouse->X -= 1;
+        warehouse->Y -= 1;
+        warehouse->Z -= 1;
+    }
+}
+
 int main(){
     // Initialize shared memory
     int shm_id = init_shared_memory(); 
     Warehouse* warehouse = (Warehouse*)shmat(shm_id, nullptr, 0);
 
     while(true){
-        warehouse->X += -1;
-        warehouse->Y += -1;
-        warehouse->Z += -1;
+        assemble_product(warehouse);
         sleep(4);
     }
 }
--- processes/supplier.cpp
+++ processes/supplier.cpp
@@ -2,15 +2,19 @@
 
 using namespace std;
 
+void supply_product(Warehouse* warehouse){
+    warehouse->X += 1;
+    warehouse->Y += 1;
+    warehouse->Z += 1;
+}
+
 int main(){
     // Initialize shared memory
     int shm_id = init_shared_memory();
     Warehouse* warehouse = (Warehouse*)shmat(shm_id, nullptr, 0);
 
     while(true){
-        warehouse->X += 1;
-        warehouse->Y += 1;
-        warehouse->Z += 1;
+        supply_product(warehouse);
         sleep(3);
     }
 }
